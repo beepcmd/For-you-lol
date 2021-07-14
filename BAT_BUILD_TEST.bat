8debf4f2e29025057dac7c8e8e00fa11660a4824
beepcmd <80488792+beepcmd@users.noreply.github.com>
2021-07-14 19:52:28 -0400

    Garbage

---
+++ BAT_BUILD_TEST.bat
@@ -0,0 +1,7 @@
+echo "# For-you-lol" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/beepcmd/For-you-lol.git
+git push -u origin main
